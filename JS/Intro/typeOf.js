0736496267262772fc5ce3a07d06a10fd5e02c24
Daveny Kuria <davenykuria@gmail.com>
2026-06-18 10:27:54 +0300

    feat(typeOf): add typeOf.js file

---
+++ JS/Intro/typeOf.js
@@ -0,0 +1,18 @@
+
+let num1 = 749;
+console.log(`num1 ${num1}, its type is ${typeof num1}`);
+
+let strNum = "8454";
+console.log(`strNum ${strNum}, its type is ${typeof strNum}`);
+
+let myName = "John";
+console.log(`My name is ${myName}, its type is ${typeof myName}`);
+
+let isMarried = true;
+console.log(`isMarried ${isMarried}, its type is ${typeof isMarried}`);
+
+let nullVal = null;
+console.log(`nullVal ${nullVal}, its type is ${typeof nullVal}`);
+
+let unValue;
+console.log(`unValue ${unValue}, its type is ${typeof unValue}`);
